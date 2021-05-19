69557a66e90d3cfbf65ddc4072f85fcfe65482df
marcos <marcosstevens2012@gmail.com>
2021-05-19 13:53:58 -0300

    paises

--- paisesApp/src/app/pais/pages/por-pais/por-pais.component.ts
+++ paisesApp/src/app/pais/pages/por-pais/por-pais.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { PaisService } from '../../services/pais.service';
+import { Country } from '../../interfaces/pais.interface';
 
 @Component({
   selector: 'app-por-pais',
@@ -9,25 +10,38 @@ import { PaisService } from '../../services/pais.service';
 })
 export class PorPaisComponent{
 
-  termino:string = 'hola mundo'
+  termino:string = 'arg'
   hayError: boolean = false;
-
+  paises: Country[] = []
 
   constructor( private PaisService: PaisService) { }
 
 
-  buscar(){
+  buscar( termino: string ){
+
     this.hayError = false 
+    this.termino = termino
+    
     this.PaisService.buscarPais( this.termino )
     .subscribe( (paises) => {
       console.log(paises);
-      
+      if (paises.length > 0) {
+        this.paises = paises
+      }
     }, (err) => {
       this.hayError = true
+      this.paises = []
+
     });
 
     console.log(this.termino)
 
+   
+
+  }
+
+  sugerencias ( termino: string ) { 
+    this.hayError = false;
   }
 
 
