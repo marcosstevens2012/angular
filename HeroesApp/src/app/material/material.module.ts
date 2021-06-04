f16a5cca7626b418bbf4976c45dea6c70999b66f
marcos <marcosstevens2012@gmail.com>
2021-06-04 16:01:38 -0300

    asdasd

--- HeroesApp/src/app/material/material.module.ts
+++ HeroesApp/src/app/material/material.module.ts
@@ -1,21 +1,30 @@
 import { NgModule } from '@angular/core';
-import { CommonModule } from '@angular/common';
-import {MatSidenavModule} from '@angular/material/sidenav';
-import {MatToolbarModule} from '@angular/material/toolbar';
-import {MatButtonModule} from '@angular/material/button';
-import {MatIconModule} from '@angular/material/icon';
-import {MatListModule} from '@angular/material/list';
+
+import { MatAutocompleteModule } from '@angular/material/autocomplete';
+import { MatButtonModule } from '@angular/material/button';
+import { MatCardModule } from '@angular/material/card';
+import { MatGridListModule } from '@angular/material/grid-list';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatInputModule } from '@angular/material/input';
+import { MatListModule } from '@angular/material/list';
+import { MatIconModule } from '@angular/material/icon';
+import { MatProgressSpinnerModule } from '@angular/material/progress-spinner';
+import { MatSidenavModule } from '@angular/material/sidenav';
+import { MatToolbarModule } from '@angular/material/toolbar';
+
 @NgModule({
-  declarations: [],
-  imports: [
-    CommonModule
-  ],
   exports: [
-    MatSidenavModule,
-    MatToolbarModule,
+    MatAutocompleteModule,
     MatButtonModule,
+    MatCardModule,
+    MatFormFieldModule,
+    MatGridListModule,
+    MatInputModule,
+    MatListModule,
     MatIconModule,
-    MatListModule
+    MatProgressSpinnerModule,
+    MatSidenavModule,
+    MatToolbarModule
   ]
 })
 export class MaterialModule { }
